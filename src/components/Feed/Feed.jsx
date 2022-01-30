d8a306d4dcd9ecbc944a5d51a2c139573362e50f
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-01-30 17:57:13 +0900

    ADD: FeedCarousel 슬라이더 추가

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,10 +1,12 @@
 import FeedHeader from "./FeedHeader/FeedHeader";
+import FeedCarousel from "./FeedCarousel/FeedCarousel";
 import "./Feed.scss";
 
 const Feed = () => {
   return (
     <section className="feed">
       <FeedHeader />
+      <FeedCarousel />
     </section>
   );
 };
