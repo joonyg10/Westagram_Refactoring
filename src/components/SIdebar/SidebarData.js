6a91740218f2aa1b1b42c3560cbbf6e9ddff5cb8
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-01-30 11:15:17 +0900

    ADD: Recommend 컴포넌트 추가

---
+++ src/components/SIdebar/SidebarData.js
@@ -0,0 +1,20 @@
+export const SidebarData = [
+  {
+    id: 1,
+    imgUrl: "./images/stories/story1.jpg",
+    username: "enna",
+    friendName: "enna",
+  },
+  {
+    id: 2,
+    imgUrl: "./images/stories/story2.jpg",
+    username: "mike",
+    friendName: "mike",
+  },
+  {
+    id: 3,
+    imgUrl: "./images/stories/story3.jpg",
+    username: "david",
+    friendName: "david",
+  },
+];
