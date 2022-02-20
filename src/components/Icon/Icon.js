c8f8b6633de40b4b6644de4de4c0626a5200ec63
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-02-20 15:41:37 +0900

    ADD: DropDown first commit

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -3,26 +3,38 @@ import {
   AiOutlinePlusSquare,
   AiOutlineHeart,
   AiOutlineCompass,
+  AiOutlineUser,
 } from "react-icons/ai";
 import { BiSearchAlt } from "react-icons/bi";
-import { FaRegPaperPlane } from "react-icons/fa";
-import { ICONS } from "../../constant/NavbarIcons";
+import { BsBookmark } from "react-icons/bs";
+import { FaRegPaperPlane, FaCog } from "react-icons/fa";
+import { MdOutlineChangeCircle } from "react-icons/md";
+import { ALLICONS } from "../../constant/NavbarIcons";
 
 function Icon({ name }) {
   console.log(name);
   switch (name) {
-    case ICONS.home:
+    case ALLICONS.home:
       return <AiFillHome size={32} />;
-    case ICONS.plane:
+    case ALLICONS.plane:
       return <FaRegPaperPlane size={32} />;
-    case ICONS.plus:
+    case ALLICONS.plus:
       return <AiOutlinePlusSquare size={32} />;
-    case ICONS.compass:
+    case ALLICONS.compass:
       return <AiOutlineCompass size={32} />;
-    case ICONS.heart:
+    case ALLICONS.heart:
       return <AiOutlineHeart size={32} />;
-    case "search":
+    case ALLICONS.search:
       return <BiSearchAlt size={28} />;
+    case ALLICONS.user:
+      return <AiOutlineUser size={24} />;
+    case ALLICONS.BsBookmark:
+      return <BsBookmark size={24} />;
+    case ALLICONS.cog:
+      return <FaCog size={24} />;
+    case ALLICONS.change:
+      return <MdOutlineChangeCircle size={24} />;
+
     default:
       return <></>;
   }
--- src/constant/NavbarIcons.js
+++ src/constant/NavbarIcons.js
@@ -1,7 +1,26 @@
+export const ALLICONS = {
+  home: "home",
+  plane: "plane",
+  plus: "plus",
+  compass: "compass",
+  heart: "heart",
+  search: "search",
+  user: "user",
+  cog: "cog",
+  change: "change",
+};
+
 export const ICONS = {
   home: "home",
   plane: "plane",
   plus: "plus",
   compass: "compass",
   heart: "heart",
+};
+
+export const DROPDOWN_ICONS = {
+  profile: "profile",
+  bookmark: "bookmark",
+  cog: "cog",
+  change: "change",
 };
