060ccf6b0ff27a963e7ac0f49d1c025b3744e426
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-02-20 14:32:54 +0900

    ADD: Comment에 drag하는 기능 추가

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import { AiOutlineHeart, AiFillCloseCircle } from "react-icons/ai";
+import HeartButton from "../HeartButton/HeartButton";
+import { AiFillCloseCircle } from "react-icons/ai";
 import { FaRegTrashAlt } from "react-icons/fa";
 import "./Comment.scss";
+
+let animationID;
 
 function Comment() {
   const [startX, setStartX] = useState(0);
@@ -9,27 +12,42 @@ function Comment() {
   const movedX = draggedX - startX;
 
   function startDrag(e) {
+    animationID = 1;
     setStartX(e.pageX);
-  }
-
-  function finishDrag(e) {
     setDraggedX(e.pageX);
   }
 
-  function calcMovedBy(e) {}
+  function updatedDraggedX(e) {
+    animationID && setDraggedX(e.pageX);
+  }
+
+  function finishDrag(e) {
+    animationID = 0;
+  }
+
+  function resetDraggedComment() {
+    setStartX(0);
+    setDraggedX(0);
+  }
 
   return (
-    <li className="comment">
-      <section className="comment__front" style={{ "--movedX": `${movedX}` }}>
+    <li className="comment" onMouseLeave={finishDrag}>
+      <section
+        className="comment__front"
+        style={{ "--movedX": `${movedX}px` }}
+        onMouseDown={startDrag}
+        onMouseMove={updatedDraggedX}
+        onMouseUp={finishDrag}
+      >
         <h4 className="comment__writer">enna</h4>
         <p className="comment__content">Comments Here</p>
-        <AiOutlineHeart size={28} />
+        <HeartButton size="sm" />
       </section>
       <section className="comment__delete">
         <div className="icon-wrapper">
           <FaRegTrashAlt size={28} />
         </div>
-        <div className="icon-wrapper">
+        <div className="icon-wrapper" onClick={resetDraggedComment}>
           <AiFillCloseCircle size={28} />
         </div>
       </section>
